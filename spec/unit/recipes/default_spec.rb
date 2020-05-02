6946c7a96a1ca2b3b50600b3d68dd9df847d37ae
elaouane <hamzagamedev@hotmail.com>
2020-05-02 22:55:54 +0100

    first commit

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook:: logstash_cookbook
+# Spec:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'logstash_cookbook::default' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'ubuntu', '18.04'
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+    it 'should install openjdk-8-jdk' do
+       expect(chef_run).to install_package "openjdk-8-jdk"
+     end
+
+    it 'should update the source list' do
+      expect(chef_run).to update_apt_update('update')
+    end
+
+    it 'should create an logstash template in /etc/logstash/conf.d/logstash.conf' do
+      expect(chef_run).to create_template('/etc/logstash/conf.d/logstash.conf')
+    end
+
+    it 'should install apt-transport-https' do
+      expect(chef_run).to install_package "apt-transport-https"
+    end
+  end
+end
---
+++ test/integration/default/default_test.rb
@@ -0,0 +1,28 @@
+# InSpec test for recipe logstash_cookbook::default
+
+# The InSpec reference, with examples and extensive documentation, can be
+# found at https://www.inspec.io/docs/reference/resources/
+
+unless os.windows?
+  # This is an example test, replace with your own test.
+  describe user('root'), :skip do
+    it { should exist }
+  end
+end
+
+# This is an example test, replace it with your own test.
+describe port(80), :skip do
+  it { should_not be_listening }
+end
+
+describe package('apt-transport-https') do
+  it { should be_installed }
+end
+
+describe package('logstash') do
+  its('version') { should cmp > '1:7.6.*' }
+end
+
+describe package('openjdk-8-jdk') do
+  it { should be_installed }
+end
